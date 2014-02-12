50957a99ec2e6f526812390e5b97e5c61f3192f0
Ken Swain <ken@kenswain.com>
2014-02-12 15:05:24 -0600

    Added the code for the first program

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -11,8 +11,12 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+     Maybe it needs a rewrite. */
+    
     return 0;
 }
 
